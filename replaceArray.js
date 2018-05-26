0bc0df6f90673f7db9cdf511b6e8637523256eac
SuperOP535 <27556391+SuperOP535@users.noreply.github.com>
2018-05-26 18:03:50 +0200

    Update replaceArray.js

--- replaceArray.js
+++ replaceArray.js
@@ -1,4 +1,4 @@
-function replaceArray = function(txt, find, replace) {
+function replaceArray(txt, find, replace) {
   for (var i = 0; i < find.length; i++) {
     txt = txt.replace(new RegExp(find[i], "g"), replace[i]);
   }
